fb7c03583b5b12c85c8937d6fc46ef647988aa84
Michael Lai <themichaellai@gmail.com>
2014-11-16 02:53:54 -0500

    Generate test code

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
-ruby '1.9.3'
+ruby '2.1.2'
 
 gem 'sinatra'
 
-gem 'sicuro'
+gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
-require 'sicuro'
+require 'json'
+
+def test_text(user_code, klass_name, cases)
+  puts "cases", cases
+  cases_string = cases.map(&:to_s).join(',')
+"""
+require 'rspec/autorun'
+
+#{user_code}
+
+def do_testing(klass, cases)
+  describe klass do
+    cases.each do |kase|
+      it kase['name'] do
+        expect(eval(kase['code'])).to be_true
+      end
+    end
+  end
+end
+
+do_testing(#{klass_name}, [#{cases_string}])
+"""
+end
 
 post '/' do
-  s = Sicuro.new
-  res = s.eval(params[:code])
-  "#{res.stderr}\n#{res.stdout}"
+  body = JSON.parse(request.body.read)
+  test_text(body["user_code"], body["class_name"], body["cases"])
 end
